a06e025d328f0790aa43c84f626d7fa1ff21bc30
Amos Barbato <amosbarbato@gmail.com>
2023-07-05 13:54:45 -0300

    First Commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiply(num1, num2) {
+    const resulted = num1 * num2;
+    console.log(`${num1} x ${num2} = ${resulted}`);
+    return resulted;
+}
+function welcome(nome) {
+    const message = `Olá ${nome}, seja bem-vindo!`;
+    console.log(message);
+    return message;
+}
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+function multiply(num1: number, num2: number): number {
+    const resulted = num1 * num2;
+    console.log(`${num1} x ${num2} = ${resulted}`);
+    return resulted;
+}
+
+function welcome(nome: string): string {
+    const message = `Olá ${nome}, seja bem-vindo!`;
+    console.log(message);
+    return message;
+}
